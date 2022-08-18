c7f03ca0acb2731a1e743bc04bda3b338777082c
afarukcali <afarukcali@gmail.com>
2022-08-18 16:43:41 +0300

    v 1.0.8

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const { dlopen } = require("node:process");
-const { constants } = require("node:os");
 const { join } = require("node:path");
 
-dlopen(module, join("./build/Release/hex-resolver.node"), constants.dlopen.RTLD_NOW);
+dlopen(module, join("./build/Release/hex-resolver.node"));
 
 module.exports;
